ba72d66767f448abef90404c823a26cc67876dfd
AlexVictorAndrade <alex.andrade@sptech.school>
2022-11-12 13:37:56 -0200

    alteracoes no banco de dados e correcoes nas telas de front

--- corinthians/site/src/database/config.js
+++ corinthians/site/src/database/config.js
@@ -3,10 +3,10 @@ var sql = require('mssql');
 
 // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
 var sqlServerConfig = {
-    server: "SEU_SERVIDOR",
-    database: "SEU_BANCO_DE_DADOS",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
+    server: "vamos_ver_o_timao",
+    database: "vamos_ver_o_timao",
+    user: "aluno",
+    password: "sptech",
     pool: {
         max: 10,
         min: 0,
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    database: "SEU_DATABASE",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
+    database: "vamos_ver_o_timao",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
