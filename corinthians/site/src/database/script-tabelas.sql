02952ea7d8051efb0fbf5c466ed3580c24cd21af
AlexVictorAndrade <alex.andrade@sptech.school>
2022-11-16 16:00:38 -0300

    aleteacoes na dashboard e api

--- corinthians/site/src/database/script-tabelas.sql
+++ corinthians/site/src/database/script-tabelas.sql
@@ -1,7 +1,3 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-
 /*
 comandos para mysql - banco local - ambiente de desenvolvimento
 */
@@ -26,7 +22,6 @@ CREATE TABLE aviso (
 );
 
 create table selects (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
 	idSelects INT PRIMARY KEY AUTO_INCREMENT,
 	fk_torcedor INT,
 	FOREIGN KEY (fk_torcedor) REFERENCES torcedor(idTorcedor),
@@ -34,21 +29,8 @@ create table selects (
 	emocao INT,
 	criancas_mulheres CHAR(3),
 	dinheiro INT,
-	visao_campo CONSTRAINT chkVisao_campo CHECK(visao_campo IN ('Horizontal', 'Vertical'))
---fazer tudo como chk constraint
+	visao_campo varchar(45),
+    CONSTRAINT chkVisao_campo CHECK(visao_campo IN ('Horizontal', 'Vertical'))
 );
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
+select*from torcedor;
--- corinthians/site/src/models/avisoModel.js
+++ corinthians/site/src/models/avisoModel.js
@@ -7,14 +7,14 @@ function listar() {
             a.id AS idAviso,
             a.titulo,
             a.descricao,
-            a.fk_usuario,
-            u.id AS idUsuario,
+            a.fk_torcedor,
+            u.id AS id_torcedor,
             u.nome,
             u.email,
             u.senha
         FROM aviso a
-            INNER JOIN usuario u
-                ON a.fk_usuario = u.id;
+            INNER JOIN torcedor u
+                ON a.fk_torcedor = u.id;
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -27,14 +27,14 @@ function pesquisarDescricao(texto) {
             a.id AS idAviso,
             a.titulo,
             a.descricao,
-            a.fk_usuario,
-            u.id AS idUsuario,
+            a.fk_torcedor,
+            u.id AS idTorcedor,
             u.nome,
             u.email,
             u.senha
         FROM aviso a
-            INNER JOIN usuario u
-                ON a.fk_usuario = u.id
+            INNER JOIN torcedor u
+                ON a.fk_torceodr = u.id
         WHERE a.descricao LIKE '${texto}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
@@ -54,18 +54,18 @@ function listarPorUsuario(idUsuario) {
             u.email,
             u.senha
         FROM aviso a
-            INNER JOIN usuario u
-                ON a.fk_usuario = u.id
-        WHERE u.id = ${idUsuario};
+            INNER JOIN tocedor u
+                ON a.fk_torcedor = u.id
+        WHERE u.id = ${idTorcedor};
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
 
-function publicar(titulo, descricao, idUsuario) {
+function publicar(titulo, descricao, idTorcedor) {
     console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function publicar(): ", titulo, descricao, idUsuario);
     var instrucao = `
-        INSERT INTO aviso (titulo, descricao, fk_usuario) VALUES ('${titulo}', '${descricao}', ${idUsuario});
+        INSERT INTO aviso (titulo, descricao, fk_torcedor) VALUES ('${titulo}', '${descricao}', ${idTorcedor});
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
