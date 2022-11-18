567e1f504e71742fc855713519d1de205a31d069
AlexVictorAndrade <alex.andrade@sptech.school>
2022-11-18 09:48:29 -0300

    mudanca nas funcoes de acesso da dashboar e inicio do front-end da area de login

--- corinthians/site/src/models/avisoModel.js
+++ corinthians/site/src/models/avisoModel.js
@@ -41,15 +41,15 @@ function pesquisarDescricao(texto) {
     return database.executar(instrucao);
 }
 
-function listarPorUsuario(idUsuario) {
+function listarPorUsuario(idTorcedor) {
     console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarPorUsuario()");
     var instrucao = `
         SELECT 
             a.id AS idAviso,
             a.titulo,
             a.descricao,
-            a.fk_usuario,
-            u.id AS idUsuario,
+            a.fk_torcedor,
+            u.id AS idTorcedor,
             u.nome,
             u.email,
             u.senha
@@ -63,7 +63,7 @@ function listarPorUsuario(idUsuario) {
 }
 
 function publicar(titulo, descricao, idTorcedor) {
-    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function publicar(): ", titulo, descricao, idUsuario);
+    console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function publicar(): ", titulo, descricao, idTorcedor);
     var instrucao = `
         INSERT INTO aviso (titulo, descricao, fk_torcedor) VALUES ('${titulo}', '${descricao}', ${idTorcedor});
     `;
