3e8209caf7b383e7d139f611785403d7935ef4f8
AlexVictorAndrade <alex.andrade@sptech.school>
2022-11-25 15:57:28 -0300

    altercao no script do banco

--- corinthians/site/src/database/script-tabelas.sql
+++ corinthians/site/src/database/script-tabelas.sql
@@ -28,6 +28,47 @@ setor varchar(45),
 fk_torcedor INT,
 	FOREIGN KEY (fk_torcedor) REFERENCES torcedor(idTorcedor));
 
+select*from usuarioSetor;
+select*from aviso;
 select*from torcedor;
 
 
+select count(fk_torcedor) as quantidade, setor from usuarioSetor  group by setor;
+
+select count(fk_torcedor) as quantidade, setor from usuarioSetor where setor = 'visitante';
+
+insert into torcedor values
+(null, 'a', 'a@', 123),
+(null, 'b', 'a@', 123),
+(null, 'c', 'a@', 123),
+(null, 'a', 'a@', 123),
+(null, 'a', 'a@', 123),
+(null, 'a', 'a@', 123),
+(null, 'a', 'a@', 123),
+(null, 'a', 'a@', 123),
+(null, 'a', 'a@', 123);
+
+
+insert into usuarioSetor values
+(null , 'norte' , '3'),
+(null , 'sul' , '4'),
+(null , 'leste' , '5'),
+(null , 'oeste' , '6'),
+(null , 'oesteB' , '7'),
+(null , 'visitante' , '8'),
+(null , 'norte' , '9'),
+(null , 'leste' , '10'),
+(null , 'norte' , '11');
+
+SELECT 
+            a.idAviso AS idAviso,
+            a.titulo,
+            a.descricao,
+            a.fk_torcedor,
+            u.idTorcedor AS id_torcedor,
+            u.nome,
+            u.email,
+            u.senha
+        FROM aviso a
+            INNER JOIN torcedor u
+                ON a.fk_torcedor = u.idTorcedor;
